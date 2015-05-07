677d40a10b9b572c955d9eb8317d3d9f21244248
Manish Patel <maddypatel@gmail.com>
2015-05-06 23:31:05 -0400

    Added harder version

--- word_frequency.py
+++ word_frequency.py
@@ -6,17 +6,36 @@ def read_file(input_file):
         removed"""
     with open(input_file) as file:
         lines = file.read().lower()
-        lines = re.sub(r'[\s]+', ' ', lines) 
+        lines = re.sub(r'[\s]+', ' ', lines)
         lines = re.sub(r'[^A-Za-z ]', '', lines)
     return lines
 
 def word_frequency(text):
-    """ Takes the string and splits it into list
-        Gets the word frequency"""
+    """ Takes the string and splits it into list.
+        Eliminates common words from the count and
+        returns dictionary of word frequency"""
+    common_words = ['a','able','about','across','after','all','almost',
+                    'also','am','among','an','and','any','are','as','at',
+                    'be','because','been','but','by','can','cannot',
+                    'could','dear','did','do','does','either','else',
+                    'ever','every','for','from','get','got','had','has',
+                    'have','he','her','hers','him','his','how','however',
+                    'i','if','in','into','is','it','its','just','least',
+                    'let','like','likely','may','me','might','most',
+                    'must','my','neither','no','nor','not','of','off',
+                    'often','on','only','or','other','our','own','rather',
+                    'said','say','says','she','should','since','so',
+                    'some','than','that','the','their','them','then',
+                    'there','these','they','this','tis','to','too','twas',
+                    'us','wants','was','we','were','what','when','where',
+                    'which','while','who','whom','why','will','with',
+                    'would','yet','you','your']
     word_count = {}
     text = text.split()
     for word in text:
-        if word in word_count:
+        if word in common_words:
+            pass
+        elif word in word_count:
             word_count[word] += 1
         else:
             word_count[word] = 1
@@ -29,7 +48,7 @@ def sorted_dict(my_dict):
 
 def print_dict(my_dict):
     for i in my_dict:
-        print("{}: {}".format(i[0], i[1]))
+        print("{0:12} {1:100}".format(i[0], int(i[1]/4) * '#'))
 
 if __name__ == "__main__":
     my_string = read_file("sample.txt")
